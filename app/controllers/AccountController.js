6de3035061290e730689096991ce2ba9120f1694
Valentina Orozco <valentina.orozco.6911@eam.edu.co>
2021-04-18 23:55:34 -0500

    otros puntos del taller

--- app/controllers/AccountController.js
+++ app/controllers/AccountController.js
@@ -32,7 +32,59 @@ AccountController.createAccount = async (req, res, next) => {
     }
 }
 
+AccountController.delete = async (req, res, next) => {
+    //extrayendo los pathParams de la petición
+    const params = req.params;
 
+    try {
+        //se supone que el id llega así /customers/:id (acá no es con {} sino con : )
+        //await (ya que el método es async) para esperar a que termine.
+        await AccountService.delete(params.id)
+
+        //enviando respuesta al cliente (postman por ejemplo)
+        res.send({message: 'account deleted'})
+        //------------------------------------------
+    } catch(error) {//manejando las excepciones
+        console.log({error});
+        //retornando al cliente (postman por ejemplo) el error
+        res.status(500).send({error: error.message}).end();
+        next(error);
+    }
+}
+
+AccountController.removeAmountAccount = async (req, res, next) => {
+    const params = req.params;
+    //extrayendo el body de la petición
+    const body = req.body;
+
+    try {
+        await AccountService.removeAmountAccount(params.id, params.amount)
+
+        res.send({message: 'account updated'})
+        //-------------------------------------------
+    }catch(error) {
+        console.log({error});
+        res.status(500).send({error: error.message}).end();
+        next(error);
+    }
+}
+
+AccountController.addAmountAccount = async (req, res, next) => {
+    const params = req.params;
+    //extrayendo el body de la petición
+    const body = req.body;
+
+    try {
+        await AccountService.addAmountAccount(params.id, params.amount)
+
+        res.send({message: 'account updated'})
+        //-------------------------------------------
+    }catch(error) {
+        console.log({error});
+        res.status(500).send({error: error.message}).end();
+        next(error);
+    }
+}
 //eliminar cliente
 //editar cliente
 //listar cuentas de clientes
--- app/services/AccountService.js
+++ app/services/AccountService.js
@@ -33,9 +33,61 @@ AccountService.create = async (account) => {
     account.openedat = new Date();//colocando fecha de apertura
     account.amount = 0; //colocando el saldo inical
     await AccountRepository.create(account)
+}
+
+//Eliminar una cuenta, sólo si no tiene saldo
+AccountService.delete = async (idAccount) => {
+    /*consultar cuentas, se usa el await porque 
+    hay que esperar el resultado para continuar*/
+    const accounts = await AccountRepository.findById(idAccount)
+    //si el la lista es > 0 es porque tiene cuentas
+    //es necesario enviar una exception
+    if (accounts.length === 0) {
+        throw new Error ('This account does not exist')
+    }
+
+    if (accounts[0].amount !== 0) {
+        throw new Error ('You cannot eliminate this account')
+    }
+    //se elimina el cliente
+    await AccountRepository.delete(idAccount)
+}
+
+AccountService.removeAmountAccount = async (idAccount,amount) => {
+    const account = await AccountRepository.findById(idAccount)
+    //si el la lista es > 0 es porque tiene cuentas
+    //es necesario enviar una exception
+    if (account.length === 0) {
+        throw new Error ('This account does not exist')
+    }
+
+    if (amount > account[0].amount) {
+        throw new Error ('insufficient balance')
+    }
+
+    account[0].amount = account[0].amount - amount;
+
+    await AccountRepository.edit(account[0].id, account[0])
 
 }
 
+AccountService.addAmountAccount = async (idAccount,amount) => {
+    const account = await AccountRepository.findById(idAccount)
+    //si el la lista es > 0 es porque tiene cuentas
+    //es necesario enviar una exception
+    if (account.length === 0) {
+        throw new Error ('This account does not exist')
+    }
+
+    // No supe como sumar en javascript profe, que pena en todo caso D:
+    var num1 = account[0].amount;
+    var num2 = amount;
+    var suma = num1 + num2;
+    account[0].amount = suma;
+
+    await AccountRepository.edit(account[0].id, account[0])
+
+}
 
 
 
